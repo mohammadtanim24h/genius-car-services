a5896e474bcf70e450b51d01ae5de536fc0b60e9
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-14 10:50:42 +0600

    added auth redirect

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
-    console.log(serviceId);
     return (
         <div>
             <h2>This is Service: {serviceId}</h2>
+            <div className="text-center my-4">
+                <Link to="/checkout">
+                    <button className="btn btn-outline-primary">
+                        Proceed to Checkout
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./Header.css";
 import logo from "../../../images/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    useEffect(() => {
+        if (user) {
+            navigate(from, { replace: true });
+        }
+    }, [user]);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    };
+
     return (
-        <Navbar collapseOnSelect sticky="top" expand="lg" bg="primary" variant="dark">
+        <Navbar
+            collapseOnSelect
+            sticky="top"
+            expand="lg"
+            bg="primary"
+            variant="dark"
+        >
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     <img src={logo} height={30} alt="" />
@@ -36,10 +60,21 @@ const Header = () => {
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link> 
-                        <Nav.Link as={Link} to="/login">
-                            Login
+                        <Nav.Link as={Link} to="/about">
+                            About
                         </Nav.Link>
+                        {user ? (
+                            <button
+                                onClick={handleSignOut}
+                                className="btn btn-danger ms-3"
+                            >
+                                Logout
+                            </button>
+                        ) : (
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
